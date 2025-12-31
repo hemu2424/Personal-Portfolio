6c68872e5ddbbd42b1dfb0be5a80982034411605
Himanshu Poriya <hporiya06@gmail.com>
2025-12-31 10:41:04 +0530

    update resume viewer layout

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -11,14 +11,14 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-const resumeLink = `https://github.com/hemu2424/Personal-Portfolio/blob/main/src/assets/6_Himanshu_Resume.pdf`
+const resumeLink = "https://raw.githubusercontent.com/hemu2424/Personal-Portfolio/main/src/assets/6_Himanshu_Resume.pdf";
 
 
 const Resume = () => {
   const [width, setWidth] = useState(1200);
 
   useEffect(() => {
-    
+
     setWidth(window.innerWidth);
   }, []);
 
@@ -26,15 +26,8 @@ const Resume = () => {
     <div>
       <Container fluid className="resume-section">
         <Particle />
-        
 
-        <Row className="resume">
-          <Document file={pdf} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-          </Document>
-        </Row>
-
-        <Row style={{ justifyContent: "center", position: "relative" }}>
+         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
             href={resumeLink}
@@ -46,6 +39,18 @@ const Resume = () => {
             &nbsp;Download Resume
           </Button>
         </Row>
+        <Row className="resume">
+          <Document
+            file={pdf}
+            className="d-flex flex-column align-items-center gap-3"
+          >
+            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+            <Page pageNumber={2} scale={width > 786 ? 1.7 : 0.6} />
+          </Document>
+
+        </Row>
+
+       
       </Container>
     </div>
   )
